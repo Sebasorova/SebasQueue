3e5ba6421d9b9c4e16e140a8947c0d9df859bfe0
SebastianSoftware <56850980+SebastianSoftware@users.noreply.github.com>
2021-07-08 13:05:45 -0500

    Update BanType.java

--- src/main/java/net/pistonmaster/pistonqueue/bungee/utils/BanType.java
+++ src/main/java/net/pistonmaster/pistonqueue/bungee/utils/BanType.java
@@ -1,8 +1,8 @@
 /*
  * #%L
- * PistonQueue
+ * SebasQueue
  * %%
- * Copyright (C) 2021 AlexProgrammerDE
+ * Copyright (C) 2021 SebastianSoftware & PistonMaster (AlexProgrammerDE)
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
  * limitations under the License.
  * #L%
  */
-package net.pistonmaster.pistonqueue.bungee.utils;
+package me.Sebasorova.sebasqueue.bungee.utils;
 
 /**
  * How shadowbanned people should be punished.
